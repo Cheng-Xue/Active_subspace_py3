5ec0ba36f2140535eefb7c1bb7fe9e7c73d8302a
u5123690 <u5123690@anu.edu.au>
2018-04-13 17:09:36 +1000

    first commit

---
+++ active_subspaces/__init__.py
@@ -0,0 +1,4 @@
+''' This is the init file.'''
+
+
+import active_subspaces.utils as utils, active_subspaces.domains, active_subspaces.gradients, active_subspaces.integrals, active_subspaces.optimizers, active_subspaces.response_surfaces, active_subspaces.subspaces
